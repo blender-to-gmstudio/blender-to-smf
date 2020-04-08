ebca808fcdfb8d807c9c6f2fa89e04af318f4f78
bartteunis <bart_teunis@hotmail.com>
2020-04-08 12:59:39 +0200

    Finished adding most of the code, still requires a couple of changes here and there

--- io_export_gms_smf.py
+++ io_export_gms_smf.py
@@ -23,106 +23,126 @@ class ExportSMF(Operator, ExportHelper):
             maxlen=255,  # Max internal buffer length, longer would be clamped.
             )
 
-    # List of operator properties, the attributes will be assigned
-    # to the class instance from the operator settings before calling.
-    use_setting = BoolProperty(
-            name="Example Boolean",
-            description="Example Tooltip",
-            default=True,
-            )
-
-    type = EnumProperty(
-            name="Example Enum",
-            description="Choose between two items",
-            items=(('OPT_A', "First Option", "Description one"),
-                   ('OPT_B', "Second Option", "Description two")),
-            default='OPT_A',
-            )
-
     def execute(self, context):
+        # Write textures/images (same thing as seen from SMF)
+        texture_bytes = bytearray()
+        texture_bytes.extend(pack('B', len(bpy.data.images)))       # Number of textures
+        for tex in bpy.data.images:
+            channels = tex.channels
+            item_number = len(tex.pixels)
+            pixel_number = int(len(tex.pixels)/channels)
+            
+            texture_bytes.extend(bytearray(tex.name + "\0",'utf-8'))# Texture name
+            texture_bytes.extend(pack('HH',*tex.size[:]))           # Texture size (w,h)
+            
+            bytedata = [floor(component*255) for component in tex.pixels[:]]
+            texture_bytes.extend(pack('B'*item_number,*bytedata))
+        
+        # Write materials
+        material_bytes = bytearray()
+        material_bytes.extend(pack('B', len(bpy.data.materials)))
+        for mat in bpy.data.materials:
+            material_bytes.extend(bytearray(mat.name + "\0", 'utf-8'))# Material name
+            material_bytes.extend(pack('B',0))                        # Material type
+        
+        # Write models
+        model_bytes = bytearray()
+        for obj in [o for o in context.selected_objects if o.type=='MESH']:
+            data = obj.data
+            size = len(data.polygons) * 3 * 44
+            
+            model_bytes.extend(pack('I', size))
+            # Write vertex buffer contents
+            for face in data.polygons:
+                for loop in [data.loops[i] for i in face.loop_indices]:
+                    vert = data.vertices[loop.vertex_index]
+                    model_bytes.extend(pack('fff', *(vert.co[:])))
+                    model_bytes.extend(pack('fff', *(vert.normal[:])))
+                    model_bytes.extend(pack('ff', *(0, 0)))           # uv
+                    model_bytes.extend(pack('fff', *(loop.tangent[:])))
+                    model_bytes.extend(pack('f', 0))
+                    model_bytes.extend(pack('ffff', *(0, 0, 0, 0)))   # Bone indices (TODO)
+                    model_bytes.extend(pack('ffff', *(0, 0, 0, 0)))   # Bone weights (TODO)
+            
+            # Mat and tex name
+            mat_name = obj.material_slots[0].name
+            model_bytes.extend(bytearray(mat_name + '\0','utf-8'))    # Mat name
+            tex_name = obj.material_slots[0].material.texture_slots[0].texture.image.name
+            model_bytes.extend(bytearray(tex_name + '\0', 'utf-8'))   # Tex name
+            
+            # Visible
+            model_bytes.extend(pack('B',int(not obj.hide)))
+            
+            # Skinning info (dummy data)
+            model_bytes.extend(pack('L',0))                           # n
+        
+        # Write (the absence of) nodes and ambient color
+        node_bytes = bytearray()
+        node_bytes.extend(pack('B',0))                                # nodeTypeNum
+        node_bytes.extend(pack('B',0))                                # nodeNum
+        
+        ambient = [floor(component*255) for component in context.scene.world.ambient_color[:]]
+        node_bytes.extend(pack('BBB',*ambient))
+        
+        # Write (the absence of a) collision buffer
+        collision_buffer_bytes = bytearray()
+        collision_buffer_bytes.extend(pack('L',0))                    # colBuffSize
+        
+        # Write (the absence of a) rig
+        rig_bytes = bytearray()
+        rig_bytes.extend(pack('B',0))                                 # boneNum
+        
+        # Write (the absence of) animation
+        animation_bytes = bytearray()
+        animation_bytes.extend(pack('B',0))                           # animationNum
+        
+        # Write (the absence of) saved selections
+        saved_selections_bytes = bytearray()
+        saved_selections_bytes.extend(pack('B',0))                    # selNum
+        
+        
+        # Now build header
+        version = 7
+        header_bytes = bytearray("SnidrsModelFormat\0",'utf-8')
+        header_bytes.extend(pack('f', version))
+        
+        header_size = 79
+        
+        offsets = (
+            header_size,
+            header_size + len(texture_bytes),
+            header_size + len(texture_bytes) + len(material_bytes),
+            header_size + len(texture_bytes) + len(material_bytes) + len(model_bytes),
+            header_size + len(texture_bytes) + len(material_bytes) + len(model_bytes) + len(node_bytes),
+            header_size + len(texture_bytes) + len(material_bytes) + len(model_bytes) + len(node_bytes) + len(collision_buffer_bytes),
+            header_size + len(texture_bytes) + len(material_bytes) + len(model_bytes) + len(node_bytes) + len(collision_buffer_bytes) + len(rig_bytes),
+            header_size + len(texture_bytes) + len(material_bytes) + len(model_bytes) + len(node_bytes) + len(collision_buffer_bytes) + len(rig_bytes) + len(animation_bytes)
+        )
+        header_bytes.extend(pack('IIIIIIII', *offsets))               # texPos, matPos, modPos, nodPos, colPos, rigPos, aniPos, selPos
+        
+        placeholder_bytes = (0, 0)
+        header_bytes.extend(pack('II', *placeholder_bytes))
+        
+        no_models = len(context.selected_objects)
+        header_bytes.extend(pack('B', no_models))
+        
+        center, size = (0, 0, 0), 1
+        header_bytes.extend(pack('ffff', *(*center, size)))
+        
+        header_size = len(header_bytes)
+        #print(header_size)
+        
+        # Write everything to file
         with open(self.filepath, "wb") as file:
-            # Write header text
-            file.write(bytearray("SnidrsModelFormat\0",'utf-8'))
-            
-            # Write version
-            version = 7
-            file.write(pack('f', version))
-            
-            # Write rest of header
-            offsets = (0, 0, 0, 0, 0, 0, 0, 0)
-            file.write(pack('IIIIIIII', *offsets))                      # texPos, matPos, modPos, nodPos, colPos, rigPos, aniPos, selPos
-            
-            no_models = len(context.selected_objects)
-            file.write(pack('B', no_models))
-            
-            placeholder_bytes = (0, 0)
-            file.write(pack('II', *placeholder_bytes))
-            
-            center, size = (0, 0, 0), 1
-            file.write(pack('ffff', *(*center, size)))
-            
-            # Write textures/images (same thing as seen from SMF)
-            file.write(pack('B', len(bpy.data.images)))                 # Number of textures
-            for tex in bpy.data.images:
-                channels = tex.channels
-                item_number = len(tex.pixels)
-                pixel_number = int(len(tex.pixels)/channels)
-                
-                file.write(bytearray(tex.name + "\0",'utf-8'))          # Texture name
-                file.write(pack('HH',*tex.size[:]))                     # Texture size (w,h)
-                
-                bytedata = [floor(component*255) for component in tex.pixels[:]]
-                file.write(pack('B'*item_number,*bytedata))
-            
-            # Write materials
-            file.write(pack('B', len(bpy.data.materials)))
-            for mat in bpy.data.materials:
-                file.write(bytearray(mat.name + "\0", 'utf-8'))         # Material name
-                file.write(pack('B',0))                                 # Material type
-            
-            # Write models
-            for obj in [o for o in context.selected_objects if o.type=='MESH']:
-                data = obj.data
-                size = len(data.polygons) * 3 * 44
-                
-                file.write(pack('I', size))
-                # TODO Write vertex buffer contents here
-                for face in data.polygons:
-                    for loop in [data.loops[i] for i in face.loop_indices]:
-                        vert = data.vertices[loop.vertex_index]
-                        file.write(pack('fff', *(vert.co[:])))
-                        file.write(pack('fff', *(vert.normal[:])))
-                        file.write(pack('fff', *(loop.tangent[:])))
-                        file.write(pack('f', 0))
-                        file.write(pack('ffff', *(0, 0, 0, 0)))     # Bone indices (TODO)
-                        file.write(pack('ffff', *(0, 0, 0, 0)))     # Bone weights (TODO)
-                
-                mat_name = obj.material_slots[0].name
-                file.write(bytearray(mat_name + '\0','utf-8'))      # Mat name
-                
-                file.write(bytes(1))    # Null byte (end of texname)
-                
-                # TODO Skinning info (dummy data)
-            
-            # Write (the absence of) nodes
-            file.write(pack('B',0))                                 # nodeTypeNum
-            file.write(pack('B',0))                                 # nodeNum
-            
-            # Write ambient color
-            ambient = [floor(component*255) for component in context.scene.world.ambient_color[:]]
-            file.write(pack('BBB',*ambient))
-            
-            # Write (the absence of a) collision buffer
-            file.write(pack('L',0))                                 # colBuffSize
-            
-            # Write (the absence of a) rig
-            file.write(pack('B',0))                                 # boneNum
-            
-            # Write (the absence of) animation
-            file.write(pack('B',0))                                 # animationNum
-            
-            # Write (the absence of) saved selections
-            file.write(pack('B',0))                                 # selNum
+            file.write(header_bytes)
+            file.write(texture_bytes)
+            file.write(material_bytes)
+            file.write(model_bytes)
+            file.write(node_bytes)
+            file.write(collision_buffer_bytes)
+            file.write(rig_bytes)
+            file.write(animation_bytes)
+            file.write(saved_selections_bytes)
         
         return {'FINISHED'}
 
